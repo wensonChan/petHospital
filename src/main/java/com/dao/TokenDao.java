b723b170f26851748a5fbe78efa3e07bd031ade4
wensonChan <wensoncwc@gmail.com>
2023-12-06 01:49:33 +0800

    1

--- src/main/java/com/dao/TokenDao.java
+++ src/main/java/com/dao/TokenDao.java
@@ -1,6 +1,7 @@
 
 package com.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
@@ -10,6 +11,7 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.plugins.pagination.Pagination;
 import com.entity.TokenEntity;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * token
@@ -20,5 +22,9 @@ public interface TokenDao extends BaseMapper<TokenEntity> {
 
 	List<TokenEntity> selectListView(Pagination page,@Param("ew") Wrapper<TokenEntity> wrapper);
 
-	
+	@Insert("insert into TOKEN values (#{id},#{userid},#{username},#{tablename},#{role},#{token},#{addtime},#{expiratedtime})")
+	void insertToken(@Param("id")Long id, @Param("userid")Long userid, @Param("username")String username, @Param("tablename")String tablename, @Param("role")String role, @Param("token")String token, @Param("addtime")Date addtime,@Param("expiratedtime")Date expiratedtime);
+
+	@Select("SELECT MAX(ID) FROM WENSON.TOKEN")
+	Long selectTotal(Long userid);
 }
--- src/main/java/com/service/impl/TokenServiceImpl.java
+++ src/main/java/com/service/impl/TokenServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
@@ -27,6 +28,8 @@ import com.utils.Query;
  */
 @Service("tokenService")
 public class TokenServiceImpl extends ServiceImpl<TokenDao, TokenEntity> implements TokenService {
+    @Autowired
+    private TokenDao tokenDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -45,25 +48,26 @@ public class TokenServiceImpl extends ServiceImpl<TokenDao, TokenEntity> impleme
     @Override
     public PageUtils queryPage(Map<String, Object> params,
                                Wrapper<TokenEntity> wrapper) {
-        Page<TokenEntity> page =new Query<TokenEntity>(params).getPage();
-        page.setRecords(baseMapper.selectListView(page,wrapper));
+        Page<TokenEntity> page = new Query<TokenEntity>(params).getPage();
+        page.setRecords(baseMapper.selectListView(page, wrapper));
         PageUtils pageUtil = new PageUtils(page);
         return pageUtil;
     }
 
     @Override
-    public String generateToken(Long userid,String username, String tableName, String role) {
+    public String generateToken(Long userid, String username, String tableName, String role) {
         TokenEntity tokenEntity = this.selectOne(new EntityWrapper<TokenEntity>().eq("userid", userid).eq("role", role));
         String token = CommonUtil.getRandomString(32);
         Calendar cal = Calendar.getInstance();
         cal.setTime(new Date());
         cal.add(Calendar.HOUR_OF_DAY, 1);
-        if(tokenEntity!=null) {
+        if (tokenEntity != null) {
             tokenEntity.setToken(token);
             tokenEntity.setExpiratedtime(cal.getTime());
             this.updateById(tokenEntity);
         } else {
-            this.insert(new TokenEntity(userid,username, tableName, role, token, cal.getTime()));
+            tokenDao.insertToken(tokenDao.selectTotal(userid)+1, userid, username, tableName, role, token, new Date(), cal.getTime());
+//            this.insert(new TokenEntity(userid,username, tableName, role, token, cal.getTime()));
         }
         return token;
     }
@@ -71,7 +75,7 @@ public class TokenServiceImpl extends ServiceImpl<TokenDao, TokenEntity> impleme
     @Override
     public TokenEntity getTokenEntity(String token) {
         TokenEntity tokenEntity = this.selectOne(new EntityWrapper<TokenEntity>().eq("token", token));
-        if(tokenEntity == null || tokenEntity.getExpiratedtime().getTime()<new Date().getTime()) {
+        if (tokenEntity == null || tokenEntity.getExpiratedtime().getTime() < new Date().getTime()) {
             return null;
         }
         return tokenEntity;
